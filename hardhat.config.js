3fcce22246792f0754f484b1bc873940d409ed4b
Chester Ngo <mr.chester.ngo@gmail.com>
2021-12-09 01:20:46 +0700

    First commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+// ethers plugin required to interact with the contract
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BSC_SCAN_KEY = process.env.BSC_SCAN_KEY;
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY]
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: BSC_SCAN_KEY,
+  },
+};
